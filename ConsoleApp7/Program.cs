d3df24c078fa0d6e81a313a6346bc53efe10d9f6
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2022-07-12 18:11:19 +0300

    Add files via upload

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Володин";
+            string surname = "Артур";
+            string nickname;
+
+            Console.WriteLine(name + " " + surname);
+
+            nickname = name;
+            name = surname;
+            surname = nickname;
+
+            Console.WriteLine(name + " " + surname);
+        }
+    }
+}
